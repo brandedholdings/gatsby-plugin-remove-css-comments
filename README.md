14014c0a35768fe2f631972af7d715bb4ac405a2
Dave Maple <dave.maple@brandedholdings.com>
2019-03-03 12:47:15 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gatsby Plugin Remove CSS Comments
+
+For Gatsby 2 only.
+
+This plugin removes comments from CSS that add to extra bytes in your HTML documents and external data-href files
+
+## Versioning
+
+gatsby-plugin-remove-css-comments uses [http://semver.org/](http://semver.org/) for versioning.
+
+## Acknowledgment
+
+This project uses this webpack loader:
+
+- [purgecss](https://github.com/FullHuman/purgecss)
+- [purgecss-loader](https://github.com/americanexpress/purgecss-loader)
+- [gatsby](https://github.com/gatsbyjs/gatsby/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/gatsby-node.js
@@ -0,0 +1,93 @@
+/**
+ * @typedef {import('webpack').Configuration} WebpackConfig
+ * @typedef {import('webpack').RuleSetRule} RuleSetRule
+ * @typedef {import('webpack').RuleSetLoader} RuleSetLoader
+ */
+import path from 'path';
+
+
+const loadersRegex = /postcss-loader/;
+
+/**
+ * @typedef {import('webpack').RuleSetUseItem} RuleSetUseItem
+ * @typedef {import('webpack').RuleSetLoader} RuleSetLoader
+ */
+
+/**
+ * Returns the index of regex matched loaders. null if not found
+ * @param {RuleSetUseItem[]} useArray
+ * @param {RegExp} loaderNamesRegex
+ * @returns {number|null} number | null
+ */
+const findLoader = (useArray, loaderNamesRegex) => {
+    const index = useArray.findIndex(loaderObject => {
+        let loaderName = '';
+
+        if (typeof loaderObject === 'string') {
+            loaderName = loaderObject;
+        } else if (
+            'loader' in loaderObject &&
+            typeof loaderObject.loader === 'string'
+        ) {
+            loaderName = loaderObject.loader;
+        }
+
+        return loaderName.match(loaderNamesRegex) !== null;
+    });
+
+    return index === -1 ? null : index;
+};
+
+/**
+ * Insert given loader at the specified index
+ * @param {RuleSetUseItem[]} useArray
+ * @param {null|number} index
+ * @param {RuleSetLoader} loader
+ */
+const insertLoader = (useArray, index, loader) => {
+    if (index === null) {
+        return;
+    }
+    useArray.splice(index, 0, loader);
+};
+
+/**
+ * @param actions
+ * @param stage
+ * @param getConfig
+ * @param plugins
+ * @param userOptions
+ */
+export function onCreateWebpackConfig(
+    { actions, stage, getConfig },
+    { plugins, ...userOptions },
+) {
+    /**
+     * @type {WebpackConfig}
+     */
+    const config = getConfig();
+    const existingRules = config.module.rules;
+
+    /**
+     * @type {RuleSetLoader}
+     */
+    const stripCommentsloader = {
+        loader: path.join(__dirname, 'node_modules/remove-comments-loader/index.js'),
+        options: {
+            test: /\.(css)$/,
+        },
+    };
+
+    existingRules.forEach(rule => {
+        if (Array.isArray(rule.oneOf)) {
+            rule.oneOf.forEach(rule => {
+                if (Array.isArray(rule.use)) {
+                    const index = findLoader(rule.use, loadersRegex);
+                    insertLoader(rule.use, index, stripCommentsloader);
+                }
+            });
+        }
+    });
+
+    actions.replaceWebpackConfig(config);
+}
